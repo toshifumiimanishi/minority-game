292816cfb51f58e0ae902cf281a0e08878a2d09d
toshifumiimanishi <13635766+toshifumiimanishi@users.noreply.github.com>
2020-03-22 15:03:31 +0900

    Clound Functions の実装（#3）

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,24 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const { Nuxt } = require('nuxt');
+const app = express();
+const config = {
+  dev: false,
+  buildDir: 'nuxt',
+  build: {
+    publicPath: '/assets/'
+  }
+};
+const nuxt = new Nuxt(config);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+function handleRequest(req, res) {
+  res.set('Cache-Control', 'public, max-age=300, s-maxage=600')
+  return new Promise((resolve, reject) => {
+    nuxt.render(req, res, promise => {
+      promise.then(resolve).catch(reject)
+    })
+  })
+}
+
+app.use(handleRequest);
+exports.ssrapp = functions.https.onRequest(app);
